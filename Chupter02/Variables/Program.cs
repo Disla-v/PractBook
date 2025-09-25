ce5bde38634bacff1a6641cd53e3f85fcfbac9c2
Disla <disla.v@outlook.com>
2025-09-25 22:19:03 +0300

    secup

--- Chupter02/Variables/Program.cs
+++ Chupter02/Variables/Program.cs
@@ -6,3 +6,11 @@ dynamic something = "Ahmed";
 something = 12;
 something = new[] {3,5,6};
 Console.WriteLine($"Length is {something.Length}");
+
+int population = 66_000_000; // 66 миллионов человек в Великобритании
+double weight = 1.88; // в килограммах
+decimal price = 4.99M; // в фунтах стерлингов
+string fruit = "Apples"; // строки в двойных кавычках
+char letter = 'Z'; // символы в одиночных кавычках
+bool happy = true; // логическое значение — true или false
+
