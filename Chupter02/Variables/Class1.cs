2a43854c8e71364125012a3ef08808c0137dca7f
Disla <disla.v@outlook.com>
2025-09-27 23:42:26 +0300

    4r

---
+++ Chupter02/Variables/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Variables
+{
+    public enum Planets
+    {
+        earth = 2_000_000,
+        mars = 13_000_000,
+        venera = 1_000_000,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
--- Chupter02/Variables/Program.cs
+++ Chupter02/Variables/Program.cs
@@ -1,17 +1,24 @@
-﻿object height = 1.88;
-object name = "jhon";
-Console.WriteLine($"{height},{name} {nameof(name)}");
+﻿using Variables;
 
-dynamic something = "Ahmed";
-something = 12;
-something = new[] {3,5,6};
-Console.WriteLine($"Length is {something.Length}");
-
-int population = 66_000_000; // 66 миллионов человек в Великобритании
-double weight = 1.88; // в килограммах
-decimal price = 4.99M; // в фунтах стерлингов
-string fruit = "Apples"; // строки в двойных кавычках
-char letter = 'Z'; // символы в одиночных кавычках
-bool happy = true; // логическое значение — true или false
+int population = (Convert.ToInt32(Console.ReadLine()));
 
 Console.WriteLine(population);
+
+if (population == (int)Planets.earth) 
+{
+    Console.Clear();
+    Console.WriteLine("Земля"); 
+}else if  (population == (int)Planets.mars)
+{
+    Console.Clear();
+    Console.WriteLine("Марс"); 
+}
+else if (population == (int)Planets.venera)
+{
+    Console.Clear();
+    Console.WriteLine("Венера");
+}else 
+{
+    Console.Clear();
+    Console.WriteLine("Error uebok"); 
+}
